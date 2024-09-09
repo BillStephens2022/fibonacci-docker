f258bb934ede1022ccc3e1a2a7fe25f53412be6c
BillStephens2022 <two4onebill@yahoo.com>
2024-09-09 06:43:36 -0400

    added image to header

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import OtherPage from "./OtherPage";
 import Fib from "./Fib";
+import fibonacci from "./fibonacci.jpg";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
+          <div className="header-main">
+          <div className="image-container">
+            <img src={fibonacci} alt="Fibonacci" className="image-fib" />
+          </div>
           <h1 className="heading">Fibonacci Calculator</h1>
+          <div className="image-container">
+            <img src={fibonacci} alt="Fibonacci" className="image-fib" />
+          </div>
+          </div>
           <div class="routes">
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </div>
+          
         </header>
+
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
